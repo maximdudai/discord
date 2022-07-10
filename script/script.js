b3f9387123cafbeb66273cd36dba050d22233adb
maximdudai <maxim.dudai01@gmail.com>
2022-07-10 16:20:17 +0100

    create responsive nav-bar

---
+++ script/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let activeNav = false;
+
+const mobileButton = document.querySelector(".responsive-btn");
+mobileButton.addEventListener("click", function() {
+    document.querySelector(".home-links").classList.toggle("mobile-nav-link")
+    document.querySelector(".home-links").classList.toggle("active");
+
+    activeNav = !activeNav;
+});
+
+if(window.orientation > -1) {
+    document.querySelector("#detect-mobile").textContent = "mobile";
+}
+
+const closeNavBtn = document.querySelector(".close-nav");
+closeNavBtn.addEventListener("click", function() {
+    document.querySelector(".home-links").classList.toggle("mobile-nav-link")
+    document.querySelector(".home-links").classList.toggle("active");
+});
